dbd40f50715ef462cb52a91787db37822b01d4ef
akdsksdyd <kakalote4@gmail.com>
2023-03-02 14:35:08 +0900

    0302 14:34 first commit

---
+++ src/main/java/com/weasy/admin/WeasyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.weasy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeasyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeasyAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weasy/admin/WeasyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weasy.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeasyAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
