4e6fb10d5ae9f77b8f2bdd6aa5c447a5f2b6c0b1
kerri <kerri.shin@gmail.com>
2023-03-13 11:24:48 +0900

    jisoo6 commit

--- src/main/java/com/weasy/admin/controller/UserController.java
+++ src/main/java/com/weasy/admin/controller/UserController.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.tomcat.jni.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.weasy.admin.command.UserVO;
 import com.weasy.admin.service.UserService;
@@ -25,40 +28,29 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping("/management")
-	public String management(HttpSession session,
-						     Model model,
-						     Criteria cri) {
+	public String management(Model model,
+			 				@RequestParam(value = "permission",required=false) String permission ,
+			 				Criteria cri) {
 		
-		session.setAttribute("userName", "admin"); 
-		
-		//로그인 한 회원만 조회
-		String userId = (String)session.getAttribute("userId");
-
-		ArrayList<UserVO> list = userService.getList(userId, cri);
+		ArrayList<UserVO> list = userService.managementList(cri);
 		model.addAttribute("list", list);
 		
-		//페이지네이션 처리
-		int total = userService.getTotal(userId, cri);
-		PageVO pageVO = new PageVO(cri, total);
-		model.addAttribute("pageVO", pageVO);
+		System.out.println("실행");
 		
 		return "user/management";
 	}
 	
 	@GetMapping("/userList")
-	public String userList(HttpSession session,
-						   Model model,
+	public String userList(Model model,
 						   Criteria cri) {
 		
-		session.setAttribute("userName", "admin"); 
-		
-		//로그인 한 회원만 조회
-		String userId = (String)session.getAttribute("userId");
-		
-		ArrayList<UserVO> list = userService.memberList(userId, cri);
+		ArrayList<UserVO> list = userService.userList(cri);
 		model.addAttribute("list", list);
-		
 		return "user/userList";
 	}
+	
 
-}
+	
+};
+
+
--- src/main/resources/static/js/management_modal.js
+++ src/main/resources/static/js/management_modal.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
     modal.style.display = "none";
   }
   
+   //켜기이벤트
+  const btnModal = document.querySelectorAll(".modalOn");
+  btnModal.forEach(function(v) {
+    v.addEventListener("click", function(e) {
+      e.preventDefault();
+      window.scrollTo({top: 0, behavior:"smooth"}); //브라우저위로이동
+      document.body.style.overflow = "hidden"; //body에 들어가면 스크롤감춤
+      modalOn();
+    });
+  });
+  
   //모달창이외의창 끄기
   modal.addEventListener("click", e => {
     if(e.target.classList.contains("management_modal")) {
